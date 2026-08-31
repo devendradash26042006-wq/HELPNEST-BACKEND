ec99a5c54d62d3f409bb1580020590af708c52c8
devendradash26042006-wq <devendradash26042006-wq@users.noreply.github.com>
2026-08-31 16:15:03 +0530

    Prepare backend for Render deployment

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
   res.send("Help Nest Backend Running");
 });
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
