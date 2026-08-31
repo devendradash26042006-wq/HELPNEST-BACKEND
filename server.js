fe2fc5451c62792fe8946a4b1078e66dc3d7f674
devendradash26042006-wq <devendradash26042006-wq@users.noreply.github.com>
2026-08-31 17:00:21 +0530

    Fix PORT configuration

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const express = require("express");
+
 require("./config/firebase");
 
 const app = express();
@@ -16,12 +18,12 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/notification", require("./routes/notificationRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 
-
-
-
 app.get("/", (req, res) => {
   res.send("Help Nest Backend Running");
 });
+
+// Port configuration
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
